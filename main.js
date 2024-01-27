e36ece896e65209de2b37b5b9fd270819fb9d8aa
Sajib <p2385553@gmail.com>
2024-01-28 00:35:26 +0600

    added portfolio files

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed(".text", {
+    strings: ["Student" , "Comedian" , "Movies Expert"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+    
+  });
+  
